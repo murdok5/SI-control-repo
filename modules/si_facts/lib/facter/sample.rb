30ceb4181b945662f1c7393b440db43cfa3a22ad
Mike Smith <mike.smith@puppet.com>
2017-07-14 08:16:44 -0700

    Update sample.rb

--- modules/si_facts/lib/facter/sample.rb
+++ modules/si_facts/lib/facter/sample.rb
@@ -1,1 +1,7 @@
-
+# https://docs.puppet.com/facter/3.6/custom_facts.html
+Facter.add(:powerstates) do
+  confine :kernel => 'Linux'
+  setcode do
+    Facter::Core::Execution.exec('cat /sys/power/states')
+  end
+end
